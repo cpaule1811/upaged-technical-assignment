ddf7a1ff17fb6a0ca14850e13c679a7bcc3c9f42
Charlie Edwards <charles.edwards1811@gmail.com>
2021-10-31 21:52:27 +1100

    added Shift Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import Navbar from "./components/Navbar";
-import './styles/App.css'
+import ShiftCard from "./components/ShiftCard"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchShiftsInvited } from "./reducers/shiftsInvitedSlice";
+import { fetchUpcomingShifts } from "./reducers/upcomingShiftsSlice";
+import "./styles/App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const { shiftsInvitedReducer, upcomingShiftsReducer } = useSelector(
+    (state) => state
+  );
+  const { upcomingShifts } = upcomingShiftsReducer;
+  const { shiftsInvited } = shiftsInvitedReducer;
+  useEffect(() => {
+    dispatch(fetchShiftsInvited());
+    dispatch(fetchUpcomingShifts(1));
+  }, [dispatch]);
+
   return (
     <div className="background">
-        <Navbar/>
+      <Navbar />
+      <div className="heading">
+        <h2>Shifts</h2>
+        <div>You've been invited</div>
+      </div>
+      <div className="shift-card-list">
+      {shiftsInvited.map((shift, i) => (
+        <ShiftCard key={i} {...shift} invited/>
+      ))}
+    </div>
+    <div className="shift-card-list">
+      {upcomingShifts.map((shift, i) => (
+        <ShiftCard key={i} {...shift} index={i}/>
+      ))}
+    </div>
     </div>
   );
 }
